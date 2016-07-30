d8742be576f2ab5f983c59c7e209ddf1e464f8cb
slavaGanzin <slava.ganzin@gmail.com>
2016-07-30 18:38:50 +0300

    -> process

---
+++ process.js
@@ -0,0 +1,31 @@
+const R            = require('ramda')
+const debug        = R.memoize(require('debug'))
+const { execSync, spawn } = require('child_process')
+const fs           = require('fs')
+
+const exec = (...args) => {
+  debug('exec')(...args)
+  let result = String(execSync(...args))
+  if (result) debug('exec:stdout')(result)
+  return result
+}
+
+const _spawn = (args, {cwd} = {cwd: '.'}) =>
+  new Promise((resolve, reject) => {
+    let [command, ..._args] = R.split(' ', args)
+    debug(args)(args, cwd)
+    let file = args.replace(/[\s\/]/g,'.')
+    let process = spawn(command, _args)
+    let [stdout,stderr] = R.map(fs.createWriteStream, [`logs/stdout-${file}`,`logs/stderr-${file}`])
+    
+    process.stdout
+      .on('data', (data)=>debug(args)("\n" + String(data)))
+      .pipe(stdout)
+    process.stderr
+      .on('data', (data)=>debug(`stderr:${args}`)("\n" + String(data)))
+      .pipe(stderr)
+      
+    process.on('close', (code) => resolve(code))
+  })
+
+module.exports = {spawn:_spawn, exec}

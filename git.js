5661ae019c16dfb91775c1d212099b60036c341d
slavaGanzin <slava.ganzin@gmail.com>
2016-08-03 01:01:41 +0300

    refactor git

---
+++ git.js
@@ -0,0 +1,31 @@
+const R      = require('ramda')
+const fs     = require('fs')
+const {exec} = require('./process')
+
+const cloneIfNotExists = ({path, cwd}) => {
+  try {
+    fs.statSync(cwd)
+  } catch (e) {
+    exec(`git clone ${path} ${cwd}`)
+  }
+}
+
+const parseStdout = R.compose(R.reject(R.isEmpty), R.split('\n'), String)
+const removeSelectedBranch = R.map(R.replace(/^(\*)?\s+/,''))
+const removeOrigin = R.map(R.replace(/^\s+origin\//,''))
+const removeHEAD = R.reject(R.test(/HEAD/))
+
+const getRemoteBranchesHEAD = cwd =>
+  removeOrigin(removeHEAD(parseStdout(exec("git branch -r -v --no-color", {cwd}))))
+  
+const getLocalBranchesHEAD = cwd =>
+  removeSelectedBranch(parseStdout(exec("git branch -v --no-color", {cwd})))
+
+const updateBranches = cwd => exec("git fetch -a", {cwd})
+
+const checkout = ({cwd, branch}) => exec(`git checkout ${branch}`, {cwd})
+const pull = cwd => exec(`git pull`, {cwd})
+
+module.exports = {
+  getRemoteBranchesHEAD, getLocalBranchesHEAD, updateBranches, cloneIfNotExists, checkout, pull
+}

7c195aac0fe2e1d127ded589fa2cf19239a2c9e6
slavaGanzin <slava.ganzin@gmail.com>
2016-08-02 23:52:23 +0300

    fix running

--- urlMatcher.js
+++ urlMatcher.js
@@ -1,22 +1,24 @@
-const R     = require('ramda')
-const debug = R.memoize(require('debug'))
+const R      = require('ramda')
+const debug  = R.memoize(require('debug'))
 const config = require('./config')
-const fs          = require('fs')
+const fs     = require('fs')
 
 let matchers = []
-let rules = []
+let rules    = []
 
 const createMatcher  = ([rule, target]) =>
   R.ifElse(R.test(new RegExp(rule, 'gi')), ()=>target, ()=>null)
 
 const updateMatchers = () => {
   let newRules = JSON.parse(fs.readFileSync(__dirname+config.rulesFile))
+  if (R.isEmpty(newRules)) debug('sever:matchers')('empty')
   if (!R.isEmpty(R.difference(newRules, rules))) {
     debug('server:matchers')(JSON.stringify(newRules, null, ' '))
   }
   rules = newRules
   matchers = R.map(createMatcher, newRules)
 }
+updateMatchers()
 setInterval(updateMatchers, config.updateMatchersInterval)
 
 const matchURL = (URL) => R.head(R.reject(R.isNil, R.ap(matchers, [URL])))

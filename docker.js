4af1fc0a474156ea8bf5b15c2266c9ce5ea492b9
Slava Ganzin <slava.ganzin@gmail.com>
2016-08-03 16:25:51 +0300

    rewrite port handling

--- docker.js
+++ docker.js
@@ -6,40 +6,40 @@ const port          = require('portastic')
 const config        = require('./config')
 
 const exposedPorts = R.compose(R.flatten, R.map(R.match(/\d+/g)), R.match(/expose.*/gi), String)
-
+const parseDockerPorts = R.compose(R.tail, R.match(/.*:(\d+)->(\d+)/))
+  
 const getFreePorts = () => port.find(config.portastic)
 
-const runImageOnPort = (image, cwd, port) => {
-  let [fromPort=8083, ...ports] = exposedPorts(fs.readFileSync(`${cwd}/Dockerfile`))
-  spawn(`docker run --interactive --detach --name ${image} --publish ${port}:${fromPort} ${image}`)
-  return port
+const runImageOnPort = (image, cwd, inPort) => {
+  let [outPort, ...ports] = exposedPorts(fs.readFileSync(`${cwd}/Dockerfile`))
+  spawn(`docker run --interactive --detach --name ${image} --publish ${inPort}:${outPort} ${image}`)
+  return inPort
 }
 
-const ps = (image, key='') => {
-  try {
-    return !R.isEmpty(exec(`docker ps ${key} | grep -E ${image}`))
-  } catch (e) {
-    return false
-  }
+const ps = (key='') => {
+  let [columns, ...images] = R.map(R.split(/\s{2,}/), R.reject(R.equals(''), R.split("\n", exec(`docker ps ${key}`))))
+  columns = R.map(x => x.toLowerCase(), columns)
+  return R.map(R.compose(R.evolve({ports: parseDockerPorts}), R.zipObj(columns)), images)
 }
-const isRunning = image => ps(image)
-const wasRunning = image => ps(image, '-a')
+
+const getRunning    = image => R.find(R.propEq('image', image), ps())
+const getWasRunning = image => R.find(R.propEq('image', image), ps('-a'))
 
 const shutdown = image =>
   exec(`docker rm -f ${image}`)
 
 const removeIfRunning = image => {
-  if (wasRunning(image)) shutdown(image) && debug('shutdown')(image)
+  if (getWasRunning(image)) shutdown(image) && debug('shutdown')(image)
 }
 
 const getImageRunPortsOrFreePorts = (image) => {
-  const clearPorts = R.map(R.replace(/.*:(\d+)->/, '$1'))
-  let ports = clearPorts(R.match(new RegExp(image+'.*->'), exec('docker ps')))
-  if (R.isEmpty(ports)) return getFreePorts()
-  debug(`${image} was running on`)(ports)
-  return Promise.resolve(ports)
+  let running = getRunning(image)
+  if (! running) return getFreePorts()
+  let [outPort, ...inPort] = running.ports
+  debug(`${image} was running on`)(outPort)
+  return Promise.resolve(outPort)
 }
 
 module.exports = {
-  runImageOnPort, isRunning, wasRunning, shutdown, removeIfRunning, getImageRunPortsOrFreePorts
+  runImageOnPort, removeIfRunning, getImageRunPortsOrFreePorts, getRunning
 }

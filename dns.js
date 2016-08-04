c1217425c85b6100f3c5729a7d35d8e46ae85a8a
Slava Ganzin <slava.ganzin@gmail.com>
2016-08-04 18:09:10 +0300

    fix dnsmasq write

--- dns.js
+++ dns.js
@@ -3,7 +3,7 @@ const {spawn, exec} = require('./process')
 let localIp
 
 const updateDnsmasq = (image) =>
-  exec(`(grep "${image}/${localIp}" /etc/dnsmasq.conf || echo "address=/${image}/127.0.0.1" >> /etc/dnsmasq.conf) && pkill dnsmasq; dnsmasq`)
+  exec(`(grep "${image}/${localIp}" /etc/dnsmasq.conf || echo "address=/${image}/${localIp}" >> /etc/dnsmasq.conf) && pkill dnsmasq; dnsmasq`)
 
 require('dns').lookup(require('os').hostname(), (err, ip, fam) => localIp = ip)
 
